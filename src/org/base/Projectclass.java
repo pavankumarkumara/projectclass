f5f0aaf6cb53754cfabafbcbe2d5fc7282262ec0
pavannauman <kpavanme@gmail.com>
2023-02-01 12:39:23 +0530

    nauman

--- src/org/base/Projectclass.java
+++ src/org/base/Projectclass.java
@@ -4,5 +4,7 @@ public class Projectclass {
 public static void main(String[] args) {
 	String s = "java";
 	System.out.println(s);
+	boolean contains = s.contains("j");
+	System.out.println(contains);
 }
 }
